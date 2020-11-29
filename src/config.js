8186b15dd3e9f8df03894ec18646e7c6056cb26f
Daniel Holmlund <agnathan@gmail.com>
2020-11-29 10:40:43 -0800

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
   s3: {
-    REGION: 'us-east-1',
+    REGION: 'us-west-2',
     BUCKET: 'notes-app-upload-fullstack',
   },
   apiGateway: {
     REGION: 'us-east-1',
-    URL: 'https://odf8otnpwa.execute-api.us-east-1.amazonaws.com/prod/',
+    URL: 'https://odf8otnpwa.execute-api.us-east-1.amazonaws.com/prod',
   },
   cognito: {
     REGION: 'us-east-1',
@@ -13,6 +13,8 @@ const config = {
     APP_CLIENT_ID: '1bjhep729vajsussq5ji08n2rg',
     IDENTITY_POOL_ID: 'us-east-1:b7523cee-e320-4e11-983e-25cc6ff37735',
   },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: 'pk_test_4HhmqYd9HiWZh4a9b0Zs6bHM004mv6EUeH',
 };
 
 export default config;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import config from './config';
+import { Amplify } from 'aws-amplify';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
